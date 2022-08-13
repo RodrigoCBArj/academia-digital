46ba180fd25b8bcaa27c07edeb3f6b23b75db6ba
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-08-13 17:40:05 -0300

    Aluno implementation

--- src/main/java/me/dio/academia/digital/controller/AlunoController.java
+++ src/main/java/me/dio/academia/digital/controller/AlunoController.java
@@ -1,9 +1,43 @@
 package me.dio.academia.digital.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.form.AlunoForm;
+import me.dio.academia.digital.entity.form.AlunoUpdateForm;
+import me.dio.academia.digital.service.impl.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl service;
+
+    @GetMapping
+    public List<Aluno> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Aluno getById(@PathVariable Long id) {
+        return service.get(id);
+    }
+
+    @PostMapping("/criar")
+    public Aluno create(@RequestBody AlunoForm form) {
+        return service.create(form);
+    }
+
+    @PostMapping("/atualizar/{id}")
+    public Aluno update(@PathVariable Long id, @RequestBody AlunoUpdateForm alunoUpdateForm) {
+        return service.update(id, alunoUpdateForm);
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
 }
--- src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -1,7 +1,10 @@
 package me.dio.academia.digital.entity.form;
 
+import lombok.Getter;
+
 import java.time.LocalDate;
 
+@Getter
 public class AlunoForm {
 
   private String nome;
